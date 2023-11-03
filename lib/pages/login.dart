c899cca6295c96aacee9501015394132ffeb2b2d
Ranjeet-K-Balobal <ranjeetbalo600@gmail.com>
2023-11-03 08:40:16 +0530

    Second commit

---
+++ lib/pages/login.dart
@@ -0,0 +1,79 @@
+import 'package:flutter/material.dart';
+
+
+class LoginPage extends StatefulWidget {
+  const LoginPage({super.key});
+
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  final TextEditingController usernameController = TextEditingController();
+  final TextEditingController passwordController = TextEditingController();
+  String selectedRole = 'Banker'; // Default role
+
+  // Function to handle the login button press
+  void _login() {
+    final username = usernameController.text;
+    final password = passwordController.text;
+
+    // Implement your authentication logic here
+    // Check if the username and password are valid
+
+    // For demonstration, we'll just print the role and credentials
+    print('Role: $selectedRole');
+    print('Username: $username');
+    print('Password: $password');
+
+    // You can add logic to navigate to the appropriate page based on the role
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Login Page'),
+      ),
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              DropdownButton<String>(
+                value: selectedRole,
+                items: <String>['Banker', 'Farmer'].map((String value) {
+                  return DropdownMenuItem<String>(
+                    value: value,
+                    child: Text(value),
+                  );
+                }).toList(),
+                onChanged: (String? newValue) {
+                setState(() {
+                    selectedRole = newValue ?? 'Banker'; // Use a default value if null
+                   });
+                },
+
+              ),
+              TextField(
+                controller: usernameController,
+                decoration: const InputDecoration(labelText: 'Username'),
+              ),
+              TextField(
+                controller: passwordController,
+                obscureText: true,
+                decoration: const InputDecoration(labelText: 'Password'),
+              ),
+              const SizedBox(height: 16.0),
+              ElevatedButton(
+                onPressed: _login,
+                child: const Text('Login'),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
